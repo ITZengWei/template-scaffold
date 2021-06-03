b055578790547cf3d1261d39c456513fed58722f
ITZengWei <2314978313@qq.com>
2021-06-03 21:45:26 +0800

    实现侧边栏菜单

--- src/components/container/container-aside.tsx
+++ src/components/container/container-aside.tsx
@@ -1,12 +1,106 @@
-import { FC, memo, useState } from 'react'
-import { Layout } from 'antd'
+import { FC, memo, ReactNode, useState } from 'react'
+import { Layout, Menu, MenuProps } from 'antd'
+import { DashboardOutlined, UserOutlined } from '@ant-design/icons'
 import { SiderProps } from 'antd/lib/layout/Sider'
 import logoImg from '../../asserts/images/ContainerAside/banner.png'
 import { ContainerAsideInner } from './style'
+import { combineURL } from '../../utils/tool'
+import config from '../../config'
+
+/** 侧边栏菜单接口 */
+export interface IAsideMenu {
+  /** 菜单id */
+  _id: string
+
+  /** 菜单标题 */
+  title: string
+
+  /** 菜单路径 */
+  path: string
+
+  /** 菜单图标 */
+  icon?: any
+
+  /** 子菜单 */
+  children?: IAsideMenu[]
+}
 
 /** 侧边栏按钮 */
 const AsideMenu: FC = memo(() => {
-  return <div></div>
+  const menuProps: MenuProps = {
+    theme: 'dark',
+    mode: 'inline',
+    onClick(info) {
+      const { key } = info
+      const pathname = combineURL(config.BASENAME, key as string)
+
+      console.log(pathname)
+    },
+  }
+
+  const menus: IAsideMenu[] = [
+    {
+      _id: '100',
+      title: '仪表盘',
+      icon: <DashboardOutlined />,
+      path: 'dashboard',
+      children: [
+        {
+          _id: '101',
+          title: '分析页',
+          path: '/analyses',
+        },
+        {
+          _id: '102',
+          title: '工作台',
+          path: '/workplace',
+        },
+      ],
+    },
+    {
+      _id: '200',
+      title: '用户页',
+      icon: <UserOutlined />,
+      path: '/account',
+      children: [
+        {
+          _id: '201',
+          title: '个人中心',
+          path: '/center',
+        },
+        {
+          _id: '202',
+          title: '个人设置',
+          path: '/setting',
+        },
+      ],
+    },
+  ]
+
+  function renderChildrenMenu(navList: IAsideMenu[]): ReactNode {
+    /** 转换为 Menu */
+    return navList.map(nav => {
+      const { title, path, icon, children } = nav
+
+      /** 如果有多级路由 */
+      if (children?.length) {
+        return (
+          <Menu.SubMenu title={title} icon={icon} key={path}>
+            {renderChildrenMenu(children)}
+          </Menu.SubMenu>
+        )
+      }
+
+      /** 单层路由 */
+      return (
+        <Menu.Item key={path} icon={icon}>
+          {title}
+        </Menu.Item>
+      )
+    })
+  }
+
+  return <Menu {...menuProps}>{renderChildrenMenu(menus)}</Menu>
 })
 
 const ContainerAside: FC = memo(props => {
